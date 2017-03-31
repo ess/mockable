606c2b3e90461a8506b0ffd2de64c33c56f8a01e
Dennis Walters <dwalters@engineyard.com>
2017-03-31 13:20:30 -0400

    Added enable/disable
    When writing unit tests for functions that use mockable, it's
    really helpul to be able to turn mocking on/off to ensure that
    the code in question does the proper thing under both conditions.

--- mockable_test.go
+++ mockable_test.go
@@ -15,6 +15,10 @@ var _ = Describe("Mockable", func() {
 	Describe("Mocked", func() {
 		var result bool
 
+		AfterEach(func() {
+			Disable()
+		})
+
 		Context("when MOCKABLE is not set", func() {
 			BeforeEach(func() {
 				os.Unsetenv(mockableEnvVar)
@@ -36,6 +40,58 @@ var _ = Describe("Mockable", func() {
 
 			It("is true", func() {
 				Expect(result).To(Equal(true))
+			})
+		})
+	})
+
+	Describe("Enable", func() {
+		Context("when mocking is not enabled", func() {
+			BeforeEach(func() {
+				Disable()
+			})
+
+			It("turns mocking on", func() {
+				Expect(Mocked()).To(Equal(false))
+
+				Enable()
+
+				Expect(Mocked()).To(Equal(true))
+			})
+		})
+
+		Context("when mocking is enabled", func() {
+			var value = "sure, why not"
+
+			BeforeEach(func() {
+				os.Setenv(mockableEnvVar, value)
+			})
+
+			It("has no apparent effect", func() {
+				Enable()
+				Expect(Mocked()).To(Equal(true))
+				Expect(os.Getenv(mockableEnvVar)).To(Equal(value))
+			})
+		})
+	})
+
+	Describe("Disable", func() {
+		Context("when mocking is enabled", func() {
+			BeforeEach(func() {
+				Enable()
+			})
+
+			It("disabled mocking", func() {
+				Expect(Mocked()).To(Equal(true))
+				Disable()
+				Expect(Mocked()).To(Equal(false))
+			})
+		})
+
+		Context("when mocking is not enabled", func() {
+			It("has no apparent effect", func() {
+				Expect(Mocked()).To(Equal(false))
+				Disable()
+				Expect(Mocked()).To(Equal(false))
 			})
 		})
 	})
